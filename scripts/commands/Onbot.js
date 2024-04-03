6a40daada293d6aea85e4e478e265915321b929e
CYB3R-VILL4IN <107668513+CYB3R-VILL4IN@users.noreply.github.com>
2024-04-03 10:28:42 +0600

    Create Onbot.js

---
+++ scripts/commands/Onbot.js
@@ -0,0 +1,11 @@
+module.exports.config = {
+	name: "onbot",
+  prefix: 'awto',
+	version: "1.0.0",
+	permssion: 1,
+	credits: "HTHB",
+	description: "Bật Bot.",
+	category: "AdminBot",
+	cooldowns: 0
+        };
+module.exports.run = ({event, api}) =>api.sendMessage("ইবাদত বস এর পারমিশনে আবার অন হলাম মাইসেল্ফ ভিলেই'ন ইবাদত🔥❤️ এর মেসেঞ্জার রো'বট ",event.threadID, () =>process.enter(0))
